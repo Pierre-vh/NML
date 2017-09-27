90c491f0521002af8dbcf0523b49a01cbad3ce26
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2017-09-27 15:02:33 +0200

    Update main.cpp

--- NML/main.cpp
+++ NML/main.cpp
@@ -3,17 +3,15 @@
 #include "NML.h"
 int main(int argc, const char * argv[])
 {
-	if(reporter->isOk())
-		std::cout << "Still down." << std::endl;
 	// NML class is a class used to parse NML files. You can re-use a lot of its code to build your own class !
 	NML n;
-	//n.params.describeProcess = true;
-	NMLNode *source = n.parseFile("C:\\Users\\pierre.vanhoutryve\\OneDrive\\Projets Programmation\\Projets\\DataContainer\\Debug\\progfile\\basic.txt");
-	assert(source);
+	n.params.describeProcess = true;
+	NMLNode *source = n.parseFile("basic.txt");
+	assert(source); // Just in case something  went wrong. You can also check if an error happened with reporter->isOk() (from globals.h) or simply usinf if(n) (it does the same thing, but the syntax is less verbose)
 	std::cout << source->getName();
 	if (n) // Parsing success, we can proceed.
 	{
-		std::cout << ">The operation was a success." << std::endl;
+		std::cout << ">The file was success fully interpreted" << std::endl;
 		std::cout  << source->compileToStr() << std::endl;
 		// This will thrown an error !
 		//source->getChild("OPDIV")->getChild(LEFT)->getChild("ERROR!!!!!");
